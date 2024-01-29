86790a1665ab71fcc2639944c77cd1b6404bfebd
Eda OZDEMIR <edaozdemir@live.fr>
2024-01-29 12:53:09 +0100

    relationship models

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
 
 class Attendee extends Model
 {
     use HasFactory;
+
+    //un participant appartient à un utilisateur
+
+    public function user(): BelongsTo 
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //un participant appartient à un évènement
+
+    public function event(): BelongsTo 
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Event extends Model
 {
     use HasFactory;
+
+
+    //un évènement appartient à un utilisateur
+    public function user(): BelongsTo
+
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    //un évènement a plusieurs participants
+
+    public function attendees(): HasMany
+    {
+        return $this->hasMany(Attendee::class);
+
+    }
 }
