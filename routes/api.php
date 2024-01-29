e3cf0e3c02fe4a63624e2519ec2882a28c83cda7
Eda OZDEMIR <edaozdemir@live.fr>
2024-01-29 13:21:31 +0100

    routes events ok

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\Api\EventController;
+use App\Http\Controllers\Api\AttendeeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::apiResource('events', EventController::class);
+
+//le participant sera lié à l'event specifique sinon erreur
+Route::apiResource('events.attendees', AttendeeController::class)->scoped(['attendee' => 'event']);
